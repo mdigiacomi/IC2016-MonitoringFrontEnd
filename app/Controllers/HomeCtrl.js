b3bbb4b0c7d9c4b4f2108703d10771e4e5516a93
Michael Di Giacomi <me@michaeldigiacomi.com>
2016-10-13 21:13:39 -0400

    delete works

--- app/Controllers/HomeCtrl.js
+++ app/Controllers/HomeCtrl.js
@@ -37,47 +37,62 @@ angular.module('LS-APPMON.Home', ['ui.grid', 'ui.grid.autoResize', 'ui.grid.resi
             $scope.gridApi = gridApi;
         },
         columnDefs: [
-            { name: 'ID', field: 'id', enableCellEdit: true, visible:false, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
-            { name: 'App Name', field: 'App Name', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
-            { name: 'App Description', field: 'AppDesc', width: "200", enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
-            { name: 'App URL', field: 'AppURL', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader, cellTemplate: '<div style="padding: 5px;"><a href="{{row.entity.AppURL}}" target="_blank">App Link</a></div>' },
+            { name: 'ID', field: 'ID', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
+            { name: 'App Name', field: 'AppName', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
             { name: 'App Version', field: 'AppVersion', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
-            { name: 'Delete', field: 'id', width: "150", cellTemplate: '<div id="{{row.entity.id}}_buttonDiv"> <button ng-click="grid.appScope.deleteSystem(row.entity.id, row.entity.AppName)" class="btn gridbutton" id="deleteButton"> Delete </button> <button ng-click="grid.appScope.deleteSystem(row.entity.id, row.entity.AppName)" class="btn gridbutton" id="deleteButton"> Ping </button></div>' }
+            { name: 'App Description', field: 'AppDesc', width: "200", enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
+            { name: 'Machine Name', field: 'AdditionalParameters["Machine Name"]', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
+            { name: '.Net Version', field: 'AdditionalParameters["Net Version"]', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
+            { name: 'Service Account', field: 'AdditionalParameters["User"]', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader },
+            { name: 'App URL', field: 'AppURL', enableCellEdit: true, enableColumnMenus: false, headerCellClass: $scope.highlightFilteredHeader, cellTemplate: '<div style="padding: 5px;"><a href="{{row.entity.AppURL}}" target="_blank">App Link</a></div>' },
+            { name: 'Delete', field: 'id', width: "150", cellTemplate: '<div id="{{row.entity.ID}}_buttonDiv"> <button ng-click="grid.appScope.deleteSystem(row.entity.ID, row.entity.AppName)" class="btn gridbutton" id="deleteButton"> Delete </button> <button ng-click="grid.appScope.pingSystem(row.entity.AppName, row.entity.AppURL)" class="btn gridbutton" id="deleteButton"> Ping </button></div>' }
         ]
     };
 
     $scope.Load = function(){
         $http({
                 method: "get",
-                url: "http://dev.michaeldigiacomi.com/IC2016/ESAppHealth/api/Loader?IndexName=appinfo&IndexType=appinfo",
+                url: "http://dev.michaeldigiacomi.com/IC2016/ESAppHealth/api/appinfo?IndexName=appinfo&IndexType=appinfo",
                 headers: {
                     'content-type': 'application/json'
                 }
             })
             .success(function(data) {
-                $scope.gridOptions.data = data.SystemList;          
+                $scope.gridOptions.data = data;          
             });
     };
 
     $scope.deleteSystem = function(id, AppName){
-        var changeSet = {
-	        "SystemUpdateList":[{
-		        "id": id
-	        }]
-        };
 
          $http({
+            method: "DELETE",
+            url: "http://dev.michaeldigiacomi.com/IC2016/ESAppHealth/api/appinfo/appinfo/appinfo/" + id,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .success(function(data, returnval) {
+            $scope.Load();
+        });
+    };
+
+    $scope.pingSystem = function(AppName, AppURL){
+        var changeSet = {
+	        AppName : AppURL
+        };
+
+        $http({
             method: "post",
-            url: "MockData/SystemList.json",
+            url: "http://dev.michaeldigiacomi.com/IC2016/HealthCheck/api/HealthCheck",
             headers: {
                 'content-type': 'application/json'
             },
             data: changeSet
         })
         .success(function(data, returnval) {
-            $scope.loadData();
+            $scope.Load();
         });
-    };
+    }
 
     $scope.Load();
 });
